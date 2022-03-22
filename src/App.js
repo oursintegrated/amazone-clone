81df0e780143a697accb8957731d10a2f1eecac3
I Ketut Adi Wicaksana <adiwicaksana846@gmail.com>
2022-03-22 10:39:36 +0700

    create header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Header";
 import Home from "./Home";
 
 function App() {
   return (
     <div className="app">
+      <Header />
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
