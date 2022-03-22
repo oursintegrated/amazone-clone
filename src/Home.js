db63a16d1e29e5cd6193a1a90d2740a43152aec0
I Ketut Adi Wicaksana <adiwicaksana846@gmail.com>
2022-03-22 10:46:30 +0700

    create background home

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
+import "./Home.css";
+import AmazonBackground from "./assets/images/AmazonBackground.jpg";
 
 function Home() {
-  return <div className="home">Hello</div>;
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img className="home__image" src={AmazonBackground} alt="" />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
